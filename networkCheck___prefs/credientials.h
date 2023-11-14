cd8aca8045f1a3c560317bb8525b24e36ecf4b30
macca-nz <20883620+macca448@users.noreply.github.com>
2023-11-14 17:40:00 +1300

    Update credientials.h

--- networkCheck___prefs/credientials.h
+++ networkCheck___prefs/credientials.h
@@ -1,9 +1,9 @@
-#define BLYNK_TEMPLATE_ID "TMPL2g_id7aKJ"
-#define BLYNK_TEMPLATE_NAME "Kombucha"
-#define BLYNK_AUTH_TOKEN "HYNXA3ppl3-nWWtDzlA5CEiROjIbEmyA"
+#define BLYNK_TEMPLATE_ID "your_ID"
+#define BLYNK_TEMPLATE_NAME "your_template"
+#define BLYNK_AUTH_TOKEN "your_token"
  
 // Your WiFi credentials.
 // Set password to "" for open networks.
-const char* ssid     = "MillFlat_El_Rancho";
-const char* password = "140824500925";
+const char* ssid     = "your_SSID";
+const char* password = "your_password";
 const char* auth     = BLYNK_AUTH_TOKEN;
